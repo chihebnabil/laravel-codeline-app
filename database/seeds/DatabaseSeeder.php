8ba01c9beee919083e2f6827a6a819c0fb7b3992
chihebnabil <nabil.chiheb@247labs.com>
2018-09-23 21:57:48 +0000

    add seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,19 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+class DatabaseSeeder extends Seeder {
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run() {
+    $this->call([
+      CountriesTableSeeder::class,
+      GenresTableSeeder::class,
+      UsersTableSeeder::class,
+      FilmsTableSeeder::class,
+      CommentsTableSeeder::class,
+    ]);
+  }
 }
