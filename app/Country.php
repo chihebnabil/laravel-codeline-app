6ac98e201dde3a559f43168a20676ec2c7e36cd9
chihebnabil <nabil.chiheb@247labs.com>
2018-09-23 18:57:26 +0000

    add Country  Model & update the controller

---
+++ app/Country.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mac
+ * Date: 23/09/2018
+ * Time: 19:55
+ */
+
+namespace App;
+
+
+class Country extends Model {
+
+
+    /**
+     * @return mixed
+     */
+    public function films() {
+        return $this->hasMany(Film::class);
+    }
+}
--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -1,96 +1,117 @@
 <?php
-
 namespace App\Http\Controllers;
-
+use App\Country;
 use App\Film;
+use App\Genre;
 use Illuminate\Http\Request;
 
-class FilmController extends Controller
-{
-    public function __construct()
-    {
-        $this->middleware('auth', ['except' => ['index','show']]);
 
-    }
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-        $films = Film::all();
 
-        // load the view and pass the films
-        return view('films.index')
-            ->with('films', $films);
-    }
+
+class FilmsController extends Controller {
+    protected $perPage = 1;
+
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function create()
-    {
-        // load the create form (app/views/films/create.blade.php)
-        return view('films.create');
+    public function feed() {
+        $films = Film::latest('realease_date')
+            ->withCount('comments')
+            ->paginate($this->perPage);
+        return view('films', [
+            'films'   => $films,
+            'title'   => __("Films Catalog"),
+            'isLocal' => config('app.env') === 'local',
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Film  $film
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function show(Film $film)
-    {
-        //
+    public function create() {
+        return view('film-add-edit', [
+            'title'     => __("Create new Film"),
+            'film'      => new Film(),
+            'countries' => $this->countries(),
+            'genres'    => $this->genres(),
+        ]);
+    }
+
+
+    /**
+     * @param Film $film
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit(Film $film) {
+        return view('film-add-edit', [
+            'title'     => __("Edit Film"),
+            'film'      => $film,
+            'countries' => $this->countries(),
+            'genres'    => $this->genres(),
+        ]);
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Film  $film
-     * @return \Illuminate\Http\Response
+     * @return mixed
      */
-    public function edit(Film $film)
-    {
-        //
+    private function countries() {
+        return Country::orderBy('title')->get();
+    }
+
+
+    /**
+     * @return mixed
+     */
+    private function genres() {
+        return Genre::orderBy('name')->get();
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Film  $film
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, Film $film)
-    {
-        //
+    public function store(Request $request) {
+        /** @var Film $film */
+        $film = Film::findOrNew($request->input('id'));
+        $isNew = !$film->exists;
+        $photoRequiredRule = $film->photo ? 'nullable' : 'required';
+        $this->validate($request, [
+            'name'          => 'required|string',
+            'description'   => 'required|string',
+            'realease_date' => 'required|string|date',
+            'country'       => 'required|integer|exists:countries,id',
+            'genre'         => 'required|integer|exists:genres,id',
+            'rating'        => 'required|integer|between:1,5',
+            'ticket_price'  => 'required|numeric|min:0|max:99.99',
+            'photo'         => "$photoRequiredRule|image|mimes:jpg,jpeg,png|max:2048",
+        ]);
+        $film->name = $request->input('name');
+        $film->slug = $film->generateSlug($film->name);
+        $film->description = $request->input('description');
+        $film->realease_date = $request->input('realease_date');
+        $film->country_id = $request->input('country');
+        $film->genre_id = $request->input('genre');
+        $film->rating = $request->input('rating');
+        $film->ticket_price = $request->input('ticket_price');
+        $film->save();
+        $this->uploadImage($film, 'photo');
+        $successMessage = $isNew ? __("Film successfully created") : __("Film successfully updated");
+        return redirect(route('home'))->with('success', $successMessage);
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Film  $film
-     * @return \Illuminate\Http\Response
+     * @param string $slug
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function destroy(Film $film)
-    {
-        //
+    public function show(string $slug) {
+        $film = Film::where('slug', '=', $slug)->firstOrFail();
+        $comments = $film->comments()->oldest()->get();
+        return view('film', [
+            'title'    => $film->title(),
+            'film'     => $film,
+            'comments' => $comments,
+        ]);
     }
 }
