0f70892d52b1600ce6cc1dc6e0b970c1b5dc7702
chihebnabil <nabil.chiheb@247labs.com>
2018-09-23 13:17:43 +0000

    add film model , controller & migration

---
+++ database/migrations/2018_09_23_131059_create_films_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFilmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->date('release_date');
+            $table->int('rating');
+            $table->float('ticket_price');
+            $table->int('country');
+            $table->int('genre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
