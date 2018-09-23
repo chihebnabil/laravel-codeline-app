e39e40f928023ce907ab25cb035ce14747881287
chihebnabil <nabil.chiheb@247labs.com>
2018-09-23 21:56:23 +0000

    fix views , webpack assets

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <link rel="shortcut icon" href="{{ asset('favicon.ico?v=1') }}" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{ asset('/dist/css/app.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
 </head>
 <body class="bg-light">
 <main>
@@ -18,6 +18,10 @@
         </div>
     </div>
 </main>
-<script src="{{ asset('dist/js/app.js') }}"></script>
+<script
+        src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+<script src="{{ asset('/js/app.js') }}"></script>
 </body>
 </html>
